0e4b87c801ce093324e9c288237faf316abb6412
Kaushal <kaushu42@gmail.com>
2020-01-12 13:46:24 +0545

    importerror fixed in settings.py

--- drwintech/settings.py
+++ drwintech/settings.py
@@ -1,5 +1,6 @@
 import dj_database_url
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -123,3 +124,4 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+django_heroku.settings(locals())
